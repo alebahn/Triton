2b309e9e87be535cdeba2b149358ea5718addd93
Andrew Shaw <zeckal4@gmail.com>
2012-03-04 23:03:21 -0700

    Player Commit

---
+++ src/triton/Player.java
@@ -0,0 +1,59 @@
+package triton;
+import java.awt.*;
+
+public class Player extends Unit {
+	private int shield;
+	private int energy;
+	private Image portrait;
+	private int charges;
+	private int rechargeRate;
+	// private ArrayList<Upgrade> boughtUpgrades;
+	// private ArrayList<Upgrade> activeUpgrades;
+	// no getters and setters for these yet
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+	public int getShield() {
+		return shield;
+	}
+	public void setShield(int shield) {
+		this.shield = shield;
+	}
+	public int getEnergy() {
+		return energy;
+	}
+	public void setEnergy(int energy) {
+		this.energy = energy;
+	}
+	public Image getPortrait() {
+		return portrait;
+	}
+	public void setPortrait(Image portrait) {
+		this.portrait = portrait;
+	}
+	public int getCharges() {
+		return charges;
+	}
+	public void setCharges(int charges) {
+		this.charges = charges;
+	}
+	public int getRechargeRate() {
+		return rechargeRate;
+	}
+	public void setRechargeRate(int rechargeRate) {
+		this.rechargeRate = rechargeRate;
+	}
+	@Override
+	public void colide(Entity other) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void colisionReact(Entity other) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
