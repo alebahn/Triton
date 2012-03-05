538d9dac9e5ad47c737388c4d207639e0106aae2
Aaron Lebahn <cplusplus328@gmail.com>
2012-03-04 23:19:23 -0700

    Cleaned up Screen

--- src/triton/Screen.java
+++ src/triton/Screen.java
@@ -2,16 +2,19 @@ package triton;
 
 import java.awt.*;
 
-
 public class Screen extends Canvas {
 	private Field field;
+	private Image background;
 	// private bitmap border
-	Screen() {}
-	public void paint(Image background){	
-		
+	Screen() {
 	}
-	private void paintHud(){
-		
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paint(g);
 	}
-	
+	private void paintHud() {
+
+	}
+
 }
