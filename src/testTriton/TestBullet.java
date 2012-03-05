7bf59b3ae4a38eb8a746aee5322e3b104898fad3
Aaron Lebahn <cplusplus328@gmail.com>
2012-03-04 23:59:30 -0700

    Started tests

---
+++ src/testTriton/TestBullet.java
@@ -0,0 +1,24 @@
+package testTriton;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBullet {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFire() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testColide() {
+		fail("Not yet implemented");
+	}
+
+}
