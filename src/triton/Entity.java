26ff74df6ff38cc7db74181b3e945e60fb0aeeb2
Aaron Lebahn <cplusplus328@gmail.com>
2012-03-04 22:56:00 -0700

    Changed formatting of Screen and added accessors to Entity.

--- src/triton/Entity.java
+++ src/triton/Entity.java
@@ -4,16 +4,25 @@ import java.awt.Image;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
-public abstract class Entity
-{
-	protected Image image; 
+public abstract class Entity {
+	protected Image image;
 	protected Rectangle2D hitBox;
-	protected Point2D	location;
-	//protected Field field
+	protected Point2D location;
+	// protected Field field
 
-	public Entity() {}
+	public Entity() {
+	}
 	public abstract void tick();
-	public void move(Point2D destination) {}
+	public void move(Point2D destination) {
+	}
 	public abstract void colide(Entity other);
 	public abstract void colisionReact(Entity other);
+
+	public Image getImage() {
+		return null;
+	}
+	public boolean isCollidingWith(Entity other) {
+		return false;
+	}
+
 }
