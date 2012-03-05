ef6a40b03742877ee6a278ed4fc42f21c6dc5134
Andrew Shaw <zeckal4@gmail.com>
2012-03-04 23:49:00 -0700

    weapon gets

--- src/triton/Upgrade.java
+++ src/triton/Upgrade.java
@@ -1,5 +1,11 @@
 package triton;
+import java.awt.*;
 
 public abstract class Upgrade {
-
+	protected int cost;
+	protected Image thumbnail;
+	protected String name;
+	public boolean buy(Player buyer){
+		return true;
+	}
 }
