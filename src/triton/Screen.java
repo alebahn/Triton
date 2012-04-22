72f925ba437d968e5437a5c38645b47737285525
Andrew Shaw <zeckal4@gmail.com>
2012-04-21 23:21:40 -0600

    screen constructor stuff

--- src/triton/Screen.java
+++ src/triton/Screen.java
@@ -3,25 +3,31 @@ package triton;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.geom.Rectangle2D;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Screen extends JPanel {
 	private Field field;
 	private Image border;
 	private Image background;
+
 	public Screen() {
 		field = new Field();
+		field.add(new Player(0,new ImageIcon("SmilieShip.png").getImage(), null));
 	}
+
 	@Override
 	public void paint(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paint(g);
-		Graphics2D g2d = (Graphics2D)g;
-		for(Entity entity : field) {
+		Graphics2D g2d = (Graphics2D) g;
+		for (Entity entity : field) {
 			entity.draw(g2d);
 		}
 	}
+
 	private void paintHud() {
 
 	}
