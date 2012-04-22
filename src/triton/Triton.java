fc40c2cc754f76d45bb37ec1a1784c05f45480ba
Andrew Shaw <zeckal4@gmail.com>
2012-04-22 00:39:14 -0600

    options subclass for triton class done

--- src/triton/Triton.java
+++ src/triton/Triton.java
@@ -1,5 +1,7 @@
 package triton;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -7,10 +9,39 @@ import triton.GameMenu.MenuType;
 
 public class Triton extends JFrame
 {
+	private Options currentOptions;
+	
+	public class Options{
+		private Dimension resolution;
+		private int volume;
+		
+		public Options(){
+			setResolution(new Dimension(800,600));
+			setVolume(50);
+		}
+
+		public Dimension getResolution() {
+			return resolution;
+		}
+
+		public void setResolution(Dimension resolution) {
+			this.resolution = resolution;
+		}
+
+		public int getVolume() {
+			return volume;
+		}
+
+		public void setVolume(int volume) {
+			this.volume = volume;
+		}
+	}
+	
 	private static Triton current = new Triton();
 	private Triton() {
 		setTitle("Triton");
 		setSize(800, 600);
+		currentOptions = new Options();
 		GameMenu startup = new GameMenu(MenuType.STARTUP);
 		setResizable(false);
 		add(startup);
